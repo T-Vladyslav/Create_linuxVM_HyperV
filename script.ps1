f55722c9349dfea7a842751285a2044c5c924493
Vladyslav_T <tokhtaric_v_e@outlook.com>
2023-12-23 21:48:15 +0200

    Added copy iso switch off function

--- script.ps1
+++ script.ps1
@@ -1,11 +1,12 @@
 $vmParams = Import-Csv -Path "VMs.csv"
 $credential = Get-Credential  # "domain\user"
+$copyIso = $False
 $sharePath = '\\vd-adm-44\Install\OracleLinux-R8-U8-x86_64-dvd.iso'
 $isoDestination = 'C:\Install'
 
 foreach ($vm in $vmParams) {
     Invoke-Command -ComputerName $vm.'Parent Host Name' -Credential $credential -ScriptBlock {
-        param($vm, $credential, $sharePath, $isoDestination)
+        param($vm, $credential, $sharePath, $isoDestination, $copyIso)
 
         Write-Host "------------------------------------------------------------------------"
         Write-Host "Deploying VM $($vm.'VM Name') on the $($vm.'Parent Host Name')"
@@ -57,33 +58,38 @@ foreach ($vm in $vmParams) {
         Set-VMFirmware -VMName $vm.'VM Name' -EnableSecureBoot Off
         
         # Atach network disc and copy iso
-        $fileInfo = New-Object System.IO.FileInfo $sharePath
-        $fileName = $fileInfo.Name
-        $directoryPath = $fileInfo.DirectoryName
-        $destinationPath = "$isoDestination\$fileName"
 
-        if (Test-Path -Path $destinationPath) {
-            Write-Host -fore Green "Checking for the presence of an iso file in $isoDestination."
-            Write-Host -fore Yellow "The file already exists in the $destinationPath. Nothing to do."
-        } else {
-            Write-Host -fore Yellow "File not found in $destinationPath."
-            Write-Host -fore Green "Starting to copy..."
-            New-PSDrive -Name "W" -PSProvider "FileSystem" -Root "$directoryPath" -Credential $credential  #$Using:credential 
-            $drive = Get-PSDrive W
-            if ($drive) {
-                Write-Host -fore Green "Created network drive at $directoryPath"
-                Write-Host -fore Green "Copying the ISO file to $isoDestination..."
-                Copy-Item -Path "$sharePath" -Destination "$isoDestination"
-                remove-PSDrive -Name "W"
-                if (Test-Path -Path $destinationPath) {
-                    Write-Host -fore Green "ISO image successfully copied."
-                } else {
-                    Write-Host -fore Red "ISO image not copied."
-                }
+        if ($copyIso) {
+            $fileInfo = New-Object System.IO.FileInfo $sharePath
+            $fileName = $fileInfo.Name
+            $directoryPath = $fileInfo.DirectoryName
+            $destinationPath = "$isoDestination\$fileName"
+
+            if (Test-Path -Path $destinationPath) {
+                Write-Host -fore Green "Checking for the presence of an iso file in $isoDestination."
+                Write-Host -fore Yellow "The file already exists in the $destinationPath. Nothing to do."
             } else {
-                Write-Host -fore Red "Network drive does'n exist"
+                Write-Host -fore Yellow "File not found in $destinationPath."
+                Write-Host -fore Green "Starting to copy..."
+                New-PSDrive -Name "W" -PSProvider "FileSystem" -Root "$directoryPath" -Credential $credential  #$Using:credential 
+                $drive = Get-PSDrive W
+                if ($drive) {
+                    Write-Host -fore Green "Created network drive at $directoryPath"
+                    Write-Host -fore Green "Copying the ISO file to $isoDestination..."
+                    Copy-Item -Path "$sharePath" -Destination "$isoDestination"
+                    remove-PSDrive -Name "W"
+                    if (Test-Path -Path $destinationPath) {
+                        Write-Host -fore Green "ISO image successfully copied."
+                    } else {
+                        Write-Host -fore Red "ISO image not copied."
+                    }
+                } else {
+                    Write-Host -fore Red "Network drive does'n exist"
+                }
             }
-        }        
+        } else {
+            Write-Host -fore Green 'Copying of the iso image has been disabled by the user.'
+        }       
 
         # Setup boot from CD and map install image
         Write-Host -fore Yellow 'Setting boot from CD and mapping install image...'
@@ -95,6 +101,6 @@ foreach ($vm in $vmParams) {
         Write-Host "Deploying VM $($vm.'VM Name') on the $($vm.'Parent Host Name') completed."
         Write-Host "------------------------------------------------------------------------"
 
-    } -ArgumentList $vm, $credential, $sharePath, $isoDestination
+    } -ArgumentList $vm, $credential, $sharePath, $isoDestination, $copyIso
 }
 
